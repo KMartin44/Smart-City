99af6bb60027b20f294821d9c09b1c909debc4e6
Kovacs Martin <kovacsmartin359@gmail.com>
2026-01-07 17:09:47 +0100

    sidebar

---
+++ resources/js/components/app-sidebar.tsx
@@ -0,0 +1,114 @@
+import {
+    Home,
+    TriangleAlert,
+    CalendarDays,
+    PencilLine,
+    ChartNoAxesCombined,
+    ShieldUser,
+    MapPinned, ChevronUp, User2
+} from 'lucide-react';
+import {
+    Sidebar,
+    SidebarContent, SidebarFooter,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem
+} from '@/components/ui/sidebar';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+
+// Menu items.
+const items = [
+    {
+        title: "Home",
+        url: "#",
+        icon: Home,
+    },
+    {
+        title: "Problémabejelentés",
+        url: "#",
+        icon: TriangleAlert,
+    },
+    {
+        title: "Események",
+        url: "#",
+        icon: CalendarDays,
+    },
+    {
+        title: "Fórum",
+        url: "#",
+        icon: PencilLine,
+    },
+    {
+        title: "Statisztikák",
+        url: "#",
+        icon: ChartNoAxesCombined
+    },
+    {
+        title: "Térkép",
+        url: "#",
+        icon: MapPinned,
+    },
+    {
+        title: "Admin",
+        url: "#",
+        icon: ShieldUser,
+    }
+]
+
+export function AppSidebar() {
+    return (
+        <Sidebar variant={"sidebar"} collapsible={'icon'}>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {items.map((item) => (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild>
+                                        <a href={item.url}>
+                                            <item.icon />
+                                            <span>{item.title}</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <SidebarMenuButton>
+                                    <User2 /> Account
+                                    <ChevronUp className="ml-auto" />
+                                </SidebarMenuButton>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent
+                                side="top"
+                                className="w-[--radix-popper-anchor-width]"
+                            >
+                                <DropdownMenuItem>
+                                    <span>Login</span>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <span>Register</span>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarFooter>
+        </Sidebar>
+    )
+}
--- resources/js/layouts/mainLayout.jsx
+++ resources/js/layouts/mainLayout.jsx
@@ -1,17 +1,16 @@
-export default function MainLayout({ children }) {
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+
+export function MainLayout({ children }) {
     return (
         <div className="min-h-screen flex flex-col">
-            <header className="bg-gray-800 text-white p-4">
-                <h2>Header</h2>
-            </header>
-
-            <main className="flex-1 p-6">
-                {children}
-            </main>
-
-            <footer className="bg-gray-200 p-4 text-center">
-                <small>Footer</small>
-            </footer>
+            <SidebarProvider>
+                <AppSidebar />
+                <main className={"flex-1 p-4"}>
+                    <SidebarTrigger />
+                    {children}
+                </main>
+            </SidebarProvider>
         </div>
     );
 }
